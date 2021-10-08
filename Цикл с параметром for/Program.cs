88b61a55cf5e6328cfee7298b3ac2334cdd22311
erkova <erkova@DESKTOP-UD6B7K0>
2021-10-08 11:08:47 +0300

    Добавьте файлы проекта.

---
+++ Цикл с параметром for/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Цикл_с_параметром_for
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int n = 1, sum = 0; n <= N; n++, sum = sum + (2 * (n - 1) - 1))
+            {
+                Console.WriteLine("Квадрат числа {0} равен {1} ", n, sum + (2 * n - 1));
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Цикл с предусловием while/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Цикл_с_предусловием_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину стороны А прямоугольника: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину стороны В прямоугольника: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите длину стороны С квадрата:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int sq_a = 0;
+            int sq_b = 0;
+            while (a >= c)
+            {
+                a -= c;
+                sq_a++;
+            }
+            while (b >= c)
+            {
+                b -= c;
+                sq_b++;
+            }
+            int sum_sq = 0;
+            for (int n = 0; n < sq_a; n++)
+            {
+                sum_sq += sq_b;
+            }
+            Console.WriteLine("Количество квадратов в прямоугольнике равно {0} ", sum_sq);
+            Console.ReadKey();
+        }
+    }
+}
